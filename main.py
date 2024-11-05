f6c73afdaf059c884542c9da9ec644cb7c574def
unknown <kristina_ukr_sp@hotmail.es>
2024-11-05 12:02:49 +0100

    workshop finished

--- main.py
+++ main.py
@@ -1,8 +1,77 @@
 #imports
 
+from modules import module
 
-def main():
-  
 
+#input
+
+# configuramos el token fuera del script principal
+import os
+from dotenv import load_dotenv
+load_dotenv('.env')
+API_TOKEN = os.environ.get("API_TOKEN")
+
+# definimos las variables que forman parte del pipeline
+USERNAME = 'Kristinawk' #USERNAME
+BASE_URL = 'https://api.github.com/'
+KEY = 'repos/'
+OWNER = 'ih-datapt-mad/'
+REPO = 'dataptmad0924_labs/' #LAB_REPOSITORY
+SEARCH = 'search/issues?q=repo:'+OWNER+REPO+'+type:pr+state:{}'
+PULLS = 'pulls?page={}&per_page=100&state={}'
+COMMITS = 'pulls/{}/commits'
+STATE = 'open'
+field_list1 = ['number',
+               'title',
+               'state',
+               'created_at',
+               'updated_at',
+               'closed_at',
+               'html_url',
+               'base.repo.full_name',
+               'base.ref',
+               'head.repo.full_name',
+               'head.ref',
+               'head.repo.pushed_at']
+field_list2 = ['student_name',
+               'number',
+               'lab_name',
+               'state',
+               'lab_status',
+               'created_at',
+               'updated_at',
+               'closed_at',
+               'html_url',
+               'base.repo.full_name',
+               'base.ref',
+               'head.repo.full_name',
+               'head.ref',
+               'head.repo.pushed_at']
+field_sort1 = ['lab_status',
+               'lab_name',
+               'student_name']
+field_name1 = ['Student Name',
+               'PR Number',
+               'Lab Name',
+               'PR Status',
+               'Lab Status',
+               'PR Created at',
+               'PR Updated at',
+               'PR Closed at',
+               'PR URL',
+               'base repository',
+               'base',
+               'head repository',
+               'compare',
+               'Pushed at']
+
+#def main():
+
+
+#pipeline
 if __name__ == '__main__':
-    main()
+    #main()    
+    DF_PULLS = module.get_pulls(BASE_URL, KEY, OWNER, REPO, PULLS, SEARCH, STATE, USERNAME, API_TOKEN, field_list1)
+    DF_STATUS = module.df_status(DF_PULLS, BASE_URL, KEY, OWNER, REPO, COMMITS, USERNAME, API_TOKEN, field_list2)
+    DF_CSV = module.create_csv(DF_STATUS, field_sort1, field_name1)
+    DF_CSV
